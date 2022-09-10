c99755d5927fa4d53364b55592739f516c932708
Ayush Chauhan <79743704+ayushdev351@users.noreply.github.com>
2022-09-10 14:23:37 +0530

    Initial commit

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,19 @@
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <div className="Top"></div>
+      <div className="Mid">
+        <div className="midProjectName">Hello</div>
+        <div className="midNewProject">Hello2</div>
+      </div>
+      <div className="Main">
+        <div className="mainLeft"></div>
+        <div className="mainRight"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
