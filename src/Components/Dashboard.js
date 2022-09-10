c53b7caebc18cb3c3da2d8a6e3196f19983a75fb
Ayush Chauhan <79743704+ayushdev351@users.noreply.github.com>
2022-09-10 19:38:02 +0530

    Space Card Completed

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -8,31 +8,73 @@ function Dashboard() {
       <div className="Top"></div>
       <div className="Mid">
         <div className="midProjectName">Hello</div>
-        <div className="midNewProject">Hello2</div>
+        <button className="midNewProject">Start New project</button>
       </div>
       <div className="Main">
         <div className="mainTop"></div>
         <div className="mainStats">
           <div className="mainStatsLeft"></div>
           <div className="mainStatsRight">
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
-            <SpaceCard />
+            <SpaceCard
+              name="Cubicle/L-Type"
+              desc="This space is for a two liner short description."
+              imgUrl="https://epicofficefurniture.com.au/wp-content/uploads/2021/03/SHUSH30-1-person-cubicle-1500H-OG.jpg"
+            />
+            <SpaceCard
+              name="Linear Workstation"
+              desc="This space is for a two liner short description."
+              imgUrl="https://st2.depositphotos.com/5777248/10827/v/600/depositphotos_108272328-stock-illustration-photo-frame-icon-simple-style.jpg"
+            />
+            <SpaceCard
+              name="Linear Workstation"
+              desc="This space is for a two liner short description."
+              imgUrl="https://5.imimg.com/data5/ZK/KH/PT/SELLER-50304012/modulartwopersonlinearworkstation-500x500.png"
+            />
+            <SpaceCard
+              name="Linear Workstation"
+              desc="This space is for a two liner short description."
+              imgUrl="https://st2.depositphotos.com/5777248/10827/v/600/depositphotos_108272328-stock-illustration-photo-frame-icon-simple-style.jpg"
+            />
+            <SpaceCard
+              name="Cubicle/L-Type"
+              desc="This space is for a two liner short description."
+              imgUrl="https://epicofficefurniture.com.au/wp-content/uploads/2021/03/SHUSH30-1-person-cubicle-1500H-OG.jpg"
+            />
+            <SpaceCard
+              name="Linear Workstation"
+              desc="This space is for a two liner short description."
+              imgUrl="https://5.imimg.com/data5/ZK/KH/PT/SELLER-50304012/modulartwopersonlinearworkstation-500x500.png"
+            />
+            <SpaceCard
+              name="Linear Workstation"
+              desc="This space is for a two liner short description."
+              imgUrl="https://st2.depositphotos.com/5777248/10827/v/600/depositphotos_108272328-stock-illustration-photo-frame-icon-simple-style.jpg"
+            />
+            <SpaceCard
+              name="Linear Workstation"
+              desc="This space is for a two liner short description."
+              imgUrl="https://st2.depositphotos.com/5777248/10827/v/600/depositphotos_108272328-stock-illustration-photo-frame-icon-simple-style.jpg"
+            />
+            <SpaceCard
+              name="Linear Workstation"
+              desc="This space is for a two liner short description."
+              imgUrl="https://st2.depositphotos.com/5777248/10827/v/600/depositphotos_108272328-stock-illustration-photo-frame-icon-simple-style.jpg"
+            />
+            <SpaceCard
+              name="Linear Workstation"
+              desc="This space is for a two liner short description."
+              imgUrl="https://st2.depositphotos.com/5777248/10827/v/600/depositphotos_108272328-stock-illustration-photo-frame-icon-simple-style.jpg"
+            />
+            <SpaceCard
+              name="Linear Workstation"
+              desc="This space is for a two liner short description."
+              imgUrl="https://st2.depositphotos.com/5777248/10827/v/600/depositphotos_108272328-stock-illustration-photo-frame-icon-simple-style.jpg"
+            />
           </div>
         </div>
+      </div>
+      <div className="bottom">
+        <button className="proceedButton">Proceed</button>
       </div>
     </div>
   );
--- src/Components/SpaceCard.js
+++ src/Components/SpaceCard.js
@@ -1,11 +1,24 @@
 import "./SpaceCard.css";
 
-function Spacecard() {
+function Spacecard({ name, desc, imgUrl }) {
   return (
     <div className="card">
-      <div className="cardImage"></div>
-      <div className="cardName"></div>
-      <div className="cardDesc"></div>
+      <img className="cardImage" src={imgUrl} alt="no-input"></img>
+      <div className="cardHead">
+        <span className="cardName">{name}</span>
+        <span className="cardQty">
+          <input
+            className="cardQtyInput"
+            type="number"
+            id="quantity"
+            min="1"
+            max="99"
+          />
+        </span>
+      </div>
+      <div className="cardDesc">
+        <p>{desc}</p>
+      </div>
     </div>
   );
 }
